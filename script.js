e317c3fdd4bf901a658617bbf18826cf78360e44
Apeksha Bisen <apeksha.bisen@gmail.com>
2020-10-14 22:54:00 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+AOS.init({
+  duration: 1200,
+})
+
+$(function () {
+  $(window).on('scroll', function () {
+      if ( $(window).scrollTop() > 10 ) {
+          $('.navbar').addClass('active');
+      } else {
+          $('.navbar').removeClass('active');
+      }
+  });
+});
